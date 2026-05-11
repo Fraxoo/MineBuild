e228e1165c055d981976f337746ba4389eaedea4
Fraxoo <fraxo.officiel@gmail.com>
2026-05-11 15:58:56 +0200

    added error message

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -7,6 +7,7 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Validator\Constraints\IsTrue;
@@ -53,12 +54,14 @@ class RegistrationFormType extends AbstractType
                     ),
                 ],
             ])
-            ->add('plainPassword', PasswordType::class, [
+            ->add('plainPassword',  RepeatedType::class, [
                 // instead of being set onto the object directly,
                 // this is read and encoded in the controller
                 'mapped' => false,
-                'attr' => ['autocomplete' => 'new-password'],
-                'label' => 'Mot de passe',
+                'type' => PasswordType::class,
+                'invalid_message' => 'Les mots de passe ne correspondent pas.',
+                'first_options' => ['label' => 'Mot de passe', 'attr' => ['autocomplete' => 'new-password']],
+                'second_options' => ['label' => 'Confirmer le mot de passe', 'attr' => ['autocomplete' => 'new-password']],
                 'constraints' => [
                     new NotBlank(
                         message: 'Veuillez entrer un mot de passe',
@@ -71,23 +74,7 @@ class RegistrationFormType extends AbstractType
                     ),
                 ],
             ])
-            ->add('confirmPassword', PasswordType::class, [
-                // instead of being set onto the object directly,
-                // this is read and encoded in the controller
-                'mapped' => false,
-                'label' => 'Confirmer le mot de passe',
-                'constraints' => [
-                    new NotBlank(
-                        message: 'Veuillez entrer un mot de passe',
-                    ),
-                    new Length(
-                        min: 6,
-                        minMessage: 'Votre mot de passe doit contenir au moins {{ limit }} caractères',
-                        // max length allowed by Symfony for security reasons
-                        max: 4096,
-                    ),
-                ],
-            ])
+
         ;
     }
 
