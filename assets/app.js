de0fa51bae988a7b7a1d77f0d6c9ebd87ee11fd1
Fraxoo <fraxo.officiel@gmail.com>
2026-06-05 15:41:38 +0200

    fixed

--- assets/app.js
+++ assets/app.js
@@ -9,63 +9,67 @@ import './styles/app.css';
 
 console.log('This log comes from assets/app.js - welcome to AssetMapper! 🎉');
 
-document.querySelectorAll('[data-carousel]').forEach((carousel) => {
-    const track = carousel.querySelector('[data-carousel-track]');
-    const prevBtn = carousel.querySelector('[data-carousel-prev]');
-    const nextBtn = carousel.querySelector('[data-carousel-next]');
-    const slides = track.children;
-
-    let currentIndex = 0;
-
-    function updateCarousel() {
-        track.style.transform = `translateX(-${currentIndex * 100}%)`;
-    }
-
-    if (prevBtn) {
-        prevBtn.addEventListener('click', () => {
-            currentIndex = currentIndex === 0
-                ? slides.length - 1
-                : currentIndex - 1;
-
-            updateCarousel();
-        });
-    }
-
-    if (nextBtn) {
-        nextBtn.addEventListener('click', () => {
-            currentIndex = currentIndex === slides.length - 1
-                ? 0
-                : currentIndex + 1;
-
-            updateCarousel();
-        });
-    }
-});
+document.addEventListener('turbo:load', () => {
 
 
-document.querySelectorAll('[data-share-button]').forEach((button) => {
-    button.addEventListener('click', async () => {
-        const title = button.dataset.shareTitle;
-        const url = button.dataset.shareUrl;
+    document.querySelectorAll('[data-carousel]').forEach((carousel) => {
+        const track = carousel.querySelector('[data-carousel-track]');
+        const prevBtn = carousel.querySelector('[data-carousel-prev]');
+        const nextBtn = carousel.querySelector('[data-carousel-next]');
+        const slides = track.children;
 
-        if (navigator.share) {
-            await navigator.share({
-                title: title,
-                url: url,
-            });
+        let currentIndex = 0;
 
-            return;
+        function updateCarousel() {
+            track.style.transform = `translateX(-${currentIndex * 100}%)`;
         }
 
-        await navigator.clipboard.writeText(url);
+        if (prevBtn) {
+            prevBtn.addEventListener('click', () => {
+                currentIndex = currentIndex === 0
+                    ? slides.length - 1
+                    : currentIndex - 1;
 
-        const text = button.querySelector('span');
-        const oldText = text.textContent;
+                updateCarousel();
+            });
+        }
 
-        text.textContent = 'Lien copié';
+        if (nextBtn) {
+            nextBtn.addEventListener('click', () => {
+                currentIndex = currentIndex === slides.length - 1
+                    ? 0
+                    : currentIndex + 1;
 
-        setTimeout(() => {
-            text.textContent = oldText;
-        }, 1500);
+                updateCarousel();
+            });
+        }
     });
-});
+    
+    document.querySelectorAll('[data-share-button]').forEach((button) => {
+        button.addEventListener('click', async () => {
+            const title = button.dataset.shareTitle;
+            const url = button.dataset.shareUrl;
+
+            if (navigator.share) {
+                await navigator.share({
+                    title: title,
+                    url: url,
+                });
+
+                return;
+            }
+
+            await navigator.clipboard.writeText(url);
+
+            const text = button.querySelector('span');
+            const oldText = text.textContent;
+
+            text.textContent = 'Lien copié';
+
+            setTimeout(() => {
+                text.textContent = oldText;
+            }, 1500);
+        });
+    });
+})
+
